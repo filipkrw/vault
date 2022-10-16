f544642a7eee60a62ac3aaecfc7c7d991dd20369
Filip Krawczyk <filip@algomo.com>
2022-10-16 16:30:26 +0100

    Update config for new pet version & files cleanup

--- .pet/config.js
+++ .pet/config.js
@@ -1,20 +1,50 @@
 module.exports = {
   sources: [
     {
-      relativePath: "../snippets",
-      include: ["*"],
+      relativePath: "snippets",
     },
     {
-      relativePath: "../cheatsheets",
-      include: ["*"],
+      relativePath: "cheatsheets",
     },
     {
-      relativePath: "../links",
-      include: ["*"],
+      relativePath: "links",
     },
     {
-      relativePath: "../helpers",
-      include: ["*"],
+      relativePath: "helpers",
+    },
+  ],
+  aliases: [
+    {
+      alias: "exec:it",
+      relativePath: "snippets/docker/execInteractive",
+    },
+    {
+      alias: "git:current",
+      relativePath: "snippets/git/getCurrentBranchName",
+    },
+    {
+      alias: "git:reset:remote",
+      relativePath: "snippets/git/resetBranchToRemote",
+    },
+    {
+      alias: "git:reset:file",
+      relativePath: "snippets/git/resetFile",
+    },
+    {
+      alias: "git:pick",
+      relativePath: "snippets/git/cherryPick",
+    },
+    {
+      alias: "git:squash",
+      relativePath: "snippets/git/squash",
+    },
+    {
+      alias: "git:list:until",
+      relativePath: "snippets/git/listCommitMessagesUntilCommit",
+    },
+    {
+      alias: "git:uncommit",
+      relativePath: "snippets/git/revertLastCommit",
     },
   ],
 };
