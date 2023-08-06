127614f303d7f603c37755758de2ff605492edf7
filipkrw <filipkrw.dev@gmail.com>
2023-08-06 16:39:53 +0100

    New snippets

--- .pet/aliases.mjs
+++ .pet/aliases.mjs
@@ -64,13 +64,6 @@ export default {
       },
     },
     {
-      alias: "git:remote:delete",
-      source: {
-        type: "note",
-        relativePath: "snippets/git/deleteRemoteBranch",
-      },
-    },
-    {
       alias: "d:up",
       source: {
         type: "note",
@@ -89,20 +82,6 @@ export default {
       source: {
         type: "note",
         relativePath: "snippets/docker/stop",
-      },
-    },
-    {
-      alias: "d:restart",
-      source: {
-        type: "note",
-        relativePath: "snippets/docker/stopAndComposeUp",
-      },
-    },
-    {
-      alias: "d:reset",
-      source: {
-        type: "note",
-        relativePath: "snippets/docker/composeBuildAndUp",
       },
     },
     {
@@ -180,6 +159,301 @@ export default {
       source: {
         type: "note",
         relativePath: "snippets/starters/createAndHostHtmlPage",
+      },
+      noVariableSubstitution: true,
+    },
+    {
+      alias: "alg-1672",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1672",
+      },
+    },
+    {
+      alias: "alg-1672:get",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1672",
+      },
+    },
+    {
+      alias: "w",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-",
+      },
+    },
+    {
+      alias: "r",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-",
+      },
+    },
+    {
+      alias: "w1712",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1712",
+      },
+    },
+    {
+      alias: "r1712",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1712",
+      },
+    },
+    {
+      alias: "w1704",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1704",
+      },
+    },
+    {
+      alias: "r1704",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1704",
+      },
+    },
+    {
+      alias: "w1714",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1714",
+      },
+    },
+    {
+      alias: "r1714",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1714",
+      },
+    },
+    {
+      alias: "walex",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-alex",
+      },
+    },
+    {
+      alias: "ralex",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-alex",
+      },
+    },
+    {
+      alias: "w1720",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1720",
+      },
+    },
+    {
+      alias: "r1720",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1720",
+      },
+    },
+    {
+      alias: "w1781",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1781",
+      },
+    },
+    {
+      alias: "r1781",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1781",
+      },
+    },
+    {
+      alias: "migration-date",
+      source: {
+        type: "note",
+        relativePath: "snippets/algomo/dbMigrationDateString",
+      },
+    },
+    {
+      alias: "test:server",
+      source: {
+        type: "inline",
+        content: "docker exec server yarn workspace @algomo/server test <args*>",
+      },
+    },
+    {
+      alias: "w1790",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1790",
+      },
+    },
+    {
+      alias: "r1790",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1790",
+      },
+    },
+    {
+      alias: "w1705",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1705",
+      },
+    },
+    {
+      alias: "r1705",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1705",
+      },
+    },
+    {
+      alias: "git:delete:remote",
+      source: {
+        type: "note",
+        relativePath: "snippets/git/deleteRemoteBranch",
+      },
+    },
+    {
+      alias: "git:delete:local",
+      source: {
+        type: "note",
+        relativePath: "snippets/git/deleteLocalBranch",
+      },
+    },
+    {
+      alias: "w14.7",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-14.7",
+      },
+    },
+    {
+      alias: "r14.7",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-14.7",
+      },
+    },
+    {
+      alias: "walg",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-alg",
+      },
+    },
+    {
+      alias: "ralg",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-alg",
+      },
+    },
+    {
+      alias: "whubpop",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-hubpop",
+      },
+    },
+    {
+      alias: "rhubpop",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-hubpop",
+      },
+    },
+    {
+      alias: "git:case-rename",
+      source: {
+        type: "note",
+        relativePath: "snippets/git/renameFileCaseSensitive",
+      },
+    },
+    {
+      alias: "w1703",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1703",
+      },
+    },
+    {
+      alias: "r1703",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1703",
+      },
+    },
+    {
+      alias: "w1829",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1829",
+      },
+    },
+    {
+      alias: "r1829",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1829",
+      },
+    },
+    {
+      alias: "d:re",
+      source: {
+        type: "note",
+        relativePath: "snippets/docker/composeBuildAndUp",
+      },
+    },
+    {
+      alias: "w1863",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1863",
+      },
+    },
+    {
+      alias: "r1863",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1863",
+      },
+    },
+    {
+      alias: "w1887",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1887",
+      },
+    },
+    {
+      alias: "r1887",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1887",
+      },
+    },
+    {
+      alias: "w1853",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1853",
+      },
+    },
+    {
+      alias: "r1853",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1853",
       },
     },
   ],
