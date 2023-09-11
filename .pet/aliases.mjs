dd43489a5f5ad2838304b18330db847f563ec2ec
Filip Krawczyk <filip@algomo.com>
2023-09-11 14:59:52 +0100

    Aliases

--- .pet/aliases.mjs
+++ .pet/aliases.mjs
@@ -120,13 +120,6 @@ export default {
       },
     },
     {
-      alias: "git:rename:remote",
-      source: {
-        type: "note",
-        relativePath: "snippets/git/renameCurrentBranchRemote",
-      },
-    },
-    {
       alias: "idea",
       source: {
         type: "inline",
@@ -282,13 +275,6 @@ export default {
       },
     },
     {
-      alias: "test:server",
-      source: {
-        type: "inline",
-        content: "docker exec server yarn workspace @algomo/server test <args*>",
-      },
-    },
-    {
       alias: "w1790",
       source: {
         type: "inline",
@@ -408,13 +394,6 @@ export default {
       },
     },
     {
-      alias: "d:re",
-      source: {
-        type: "note",
-        relativePath: "snippets/docker/composeBuildAndUp",
-      },
-    },
-    {
       alias: "w1863",
       source: {
         type: "inline",
@@ -454,6 +433,440 @@ export default {
       source: {
         type: "inline",
         content: "pet df -t algomo ALG-1853",
+      },
+    },
+    {
+      alias: "w1907",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1907",
+      },
+    },
+    {
+      alias: "r1907",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1907",
+      },
+    },
+    {
+      alias: "gen:server",
+      source: {
+        type: "inline",
+        content: "docker exec server yarn workspace @algomo/server gen:all",
+      },
+    },
+    {
+      alias: "w1904",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1904",
+      },
+    },
+    {
+      alias: "r1904",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1904",
+      },
+    },
+    {
+      alias: "d:rb",
+      source: {
+        type: "note",
+        relativePath: "snippets/docker/composeBuildAndUp",
+      },
+    },
+    {
+      alias: "d:rs",
+      source: {
+        type: "inline",
+        content: "docker stop <container> && docker compose up <container> <args*>",
+      },
+    },
+    {
+      alias: "w1934",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1934",
+      },
+    },
+    {
+      alias: "r1934",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1934",
+      },
+    },
+    {
+      alias: "w1937",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1937",
+      },
+    },
+    {
+      alias: "r1937",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1937",
+      },
+    },
+    {
+      alias: "w1948",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1948",
+      },
+    },
+    {
+      alias: "r1948",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1948",
+      },
+    },
+    {
+      alias: "w1940",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1940",
+      },
+    },
+    {
+      alias: "r1940",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1940",
+      },
+    },
+    {
+      alias: "w1967",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1967",
+      },
+    },
+    {
+      alias: "r1967",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1967",
+      },
+    },
+    {
+      alias: "test:server",
+      source: {
+        type: "inline",
+        content: "docker exec server yarn workspace @algomo/server test <args*>",
+      },
+    },
+    {
+      alias: "w2003",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2003",
+      },
+    },
+    {
+      alias: "r2003",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2003",
+      },
+    },
+    {
+      alias: "w1986",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1986",
+      },
+    },
+    {
+      alias: "r1986",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1986",
+      },
+    },
+    {
+      alias: "w1946",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1946",
+      },
+    },
+    {
+      alias: "r1946",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1946",
+      },
+    },
+    {
+      alias: "w1945",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1945",
+      },
+    },
+    {
+      alias: "r1945",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1945",
+      },
+    },
+    {
+      alias: "w1981",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1981",
+      },
+    },
+    {
+      alias: "r1981",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1981",
+      },
+    },
+    {
+      alias: "w2021",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2021",
+      },
+    },
+    {
+      alias: "r2021",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2021",
+      },
+    },
+    {
+      alias: "w2010",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2010",
+      },
+    },
+    {
+      alias: "r2010",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2010",
+      },
+    },
+    {
+      alias: "wshopify",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-shopify",
+      },
+    },
+    {
+      alias: "rshopify",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-shopify",
+      },
+    },
+    {
+      alias: "w2029",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2029",
+      },
+    },
+    {
+      alias: "r2029",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2029",
+      },
+    },
+    {
+      alias: "w2038",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2038",
+      },
+    },
+    {
+      alias: "r2038",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2038",
+      },
+    },
+    {
+      alias: "w2041",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2041",
+      },
+    },
+    {
+      alias: "r2041",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2041",
+      },
+    },
+    {
+      alias: "w2046-widget-tracking-404-bug",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2046-widget-tracking-404-bug",
+      },
+    },
+    {
+      alias: "r2046-widget-tracking-404-bug",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2046-widget-tracking-404-bug",
+      },
+    },
+    {
+      alias: "w2031",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2031",
+      },
+    },
+    {
+      alias: "r2031",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2031",
+      },
+    },
+    {
+      alias: "w2047",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2047",
+      },
+    },
+    {
+      alias: "r2047",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2047",
+      },
+    },
+    {
+      alias: "w1876",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-1876",
+      },
+    },
+    {
+      alias: "r1876",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-1876",
+      },
+    },
+    {
+      alias: "w2070",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2070",
+      },
+    },
+    {
+      alias: "r2070",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2070",
+      },
+    },
+    {
+      alias: "w2061",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2061",
+      },
+    },
+    {
+      alias: "r2061",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2061",
+      },
+    },
+    {
+      alias: "regen:ps",
+      source: {
+        type: "inline",
+        content: "docker exec server yarn workspace @algomo/server gen:all && docker restart platform & docker restart server",
+      },
+    },
+    {
+      alias: "w2068",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2068",
+      },
+    },
+    {
+      alias: "r2068",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2068",
+      },
+    },
+    {
+      alias: "w2052",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2052",
+      },
+    },
+    {
+      alias: "r2052",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2052",
+      },
+    },
+    {
+      alias: "git:rename:remote",
+      source: {
+        type: "note",
+        relativePath: "snippets/git/renameCurrentBranchRemote",
+      },
+    },
+    {
+      alias: "w2053",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-2053",
+      },
+    },
+    {
+      alias: "r2053",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-2053",
+      },
+    },
+    {
+      alias: "w999",
+      source: {
+        type: "inline",
+        content: "pet dc daily -t algomo ALG-999",
+      },
+    },
+    {
+      alias: "r999",
+      source: {
+        type: "inline",
+        content: "pet df -t algomo ALG-999",
       },
     },
   ],
